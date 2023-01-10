a25c4a29fb1ae386aa0c60e0b6ee0f1440920813
BorjaMontoro <bmontoroplaza.cf@iesesteveterradas.cat>
2023-01-10 17:12:47 +0100

    Aplicar boton camara

--- app/src/main/java/com/example/capturabasicaimg/MainActivity.java
+++ app/src/main/java/com/example/capturabasicaimg/MainActivity.java
@@ -8,8 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,18 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
     public static int RC_PHOTO_PICKER = 0;
     private ActivityResultLauncher<Intent> someActivityResultLauncher;
+    private ActivityResultLauncher<Intent> someActivityResultLauncher2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button boton=findViewById(R.id.galeria);
+        Button galeria=findViewById(R.id.galeria);
+        Button camara=findViewById(R.id.camera);
         someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
-                            // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
                             ImageView imageView = findViewById(R.id.img);
@@ -37,7 +40,22 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-        boton.setOnClickListener(new View.OnClickListener(){
+        someActivityResultLauncher2= registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Bundle extras = data.getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ImageView imageView = findViewById(R.id.img);
+                            imageView.setImageBitmap(imageBitmap);
+                        }
+
+                    }
+                });
+        galeria.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 //Create Intent
@@ -46,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
                 //Launch activity to get result
                 someActivityResultLauncher.launch(intent);
+            }
+        });
+        camara.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //Create Intent
+                Intent intent2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //Launch activity to get result
+                someActivityResultLauncher2.launch(intent2);
             }
         });
     }
